738f4b2879db4bb276b1a84388042395c8f53e9b
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-05 14:45:06 -0400

    help

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,42 @@
-const settings = require('../config.json');
-exports.run = (client, message, args) => {
-  if (!args[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`[Cryptide Commands]\n\n[Use ${settings.prefix}help <command> for information!]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: 'ini'});
-  } else {
-    let command = args[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.channel.send("", {embed: {
-        color: 15400990,
-            title: `Command Help:`,
-            description: `Command Name: \`${command.help.name}\` \nDescription: ${command.help.description}\nUsage: ${settings.prefix}${command.help.usage}`,
-          }});
+exports.run = (client, message, args, level) => {
+    if (!args[0]) {
+      const settings = message.settings;
+      console.log(settings.prefixes); 
+      const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
+      const commandNames = myCommands.keyArray();
+      const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+  
+      let currentCategory = "";
+      let output = `= Command List =\n\n[Use ${settings.prefixes[0]}help <commandname> for details]\n`;
+      const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+      sorted.forEach( c => {
+        const cat = c.help.category.toProperCase();
+        if (currentCategory !== cat) {
+          output += `\n== ${cat} ==\n`;
+          currentCategory = cat;
+        }
+        output += `${settings.prefixes[0]}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+      });
+      message.channel.send(output, {code:"asciidoc"});
     } else {
-      message.channel.send("", {embed: {
-        color: 15400990,
-            title: `Non-Existant Command!`,
-            description: `I was unable to find a command by the name of: \`${args[0]}\``,
-          }});
+      // Show individual command's help.
+      let command = args[0];
+      if (client.commands.has(command)) {
+        command = client.commands.get(command);
+        if (level < client.levelCache[command.conf.permLevel]) return;
+        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:"asciidoc"});
+      }
     }
-  }
-};
-
-exports.conf = {
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'help',
-  description: 'Displays available commands.',
-  usage: 'help <command>'
-};
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: "help",
+    description: "Displays all the available commands for your permission level.",
+    usage: "help [command]"
+  };
