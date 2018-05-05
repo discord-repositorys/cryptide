e7711bbdc4590956505026fc1c8f7895d8b0e5c8
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-05 14:27:12 -0400

    fix

--- commands/ascii.js
+++ commands/ascii.js
@@ -1,7 +1,7 @@
 const figlet = require('figlet');
 
 exports.run = async (client, message, args, tools) => {
-  
+
   if(args.join(' ').length > 14) return message.channel.send('Only 14 characters are admitted!') 
   if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000})); 
     figlet(args.join(' '), (err, data) => {
