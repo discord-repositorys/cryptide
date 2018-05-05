abf2df7018ec4844dfb1a35ff1fe2c1aed713f8c
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-05 15:01:14 -0400

    a

--- events/message.js
+++ events/message.js
@@ -27,15 +27,15 @@ module.exports = (client, message) => {
 
   // Check whether the command, or alias, exist in the collections defined
   // in app.js.
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+  //const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
   // using this const varName = thing OR otherthign; is a pretty efficient
   // and clean way to grab one of 2 values!
-  if (!cmd) return;
+  //if (!cmd) return;
 
   // Some commands may not be useable in DMs. This check prevents those commands from running
   // and return a friendly error message.
-  if (cmd && !message.guild && cmd.conf.guildOnly)
-    return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
+  //if (cmd && !message.guild && cmd.conf.guildOnly)
+  //  return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
 
   if (level < client.levelCache[cmd.conf.permLevel]) {
     if (settings.systemNotice === "true") {
