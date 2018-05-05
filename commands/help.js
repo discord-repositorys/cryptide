50858f9b2f89b8e02118afa9b2d70188d7978ff9
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-05 15:08:13 -0400

    revert

--- commands/help.js
+++ commands/help.js
@@ -1,112 +1,35 @@
-const stripIndents = require('common-tags').stripIndents;
-
-exports.run = async (client, msg, args) => {
-
-    let commands = [];
-    let title = 'Categories';
-
-    if (args.length > 0) {
-        if (/^category|type$/i.test(args[0])) {
-            if (args.length < 2) {
-                throw 'You must specify a category!';
-            }
-
-            commands = client.commands.all(args[1]);
-            title = `${args[1]} Commands`;
-        } else if (/^all|full|every$/i.test(args[0])) {
-            commands = client.commands.all();
-            title = 'All Commands';
-        } else {
-            let command = client.commands.get(args[0]);
-            if (!command) {
-                throw `The command '${args[0]}' does not exist!`;
-            }
-
-            commands = [command];
-            title = `Help for \`${command.info.name}\``;
-        }
-    }
-
-    if (commands.length > 0) {
-        let fields = commands.filter(c => !c.info.hidden)
-            .sort((a, b) => a.info.name.localeCompare(b.info.name))
-            .map(c => getHelp(client, c, commands.length === 1));
-
-        // Temporary workaround for the 2k char limit
-        let maxLength = 1900;
-        let messages = [];
-
-        while (fields.length > 0) {
-            let len = 0;
-            let i = 0;
-            while (len < maxLength) {
-                if (i >= fields.length) {
-                    break;
-                }
-                let field = fields[i];
-                len += field.name.length + field.value.length;
-                if (len >= maxLength) {
-                    break;
-                }
-                i++;
-            }
-
-            messages.push({ fields: fields.splice(0, i) });
-        }
-
-        msg.delete().catch(() => { });
-        messages.map(m => m.fields).forEach(async fields => {
-            (await msg.channel.send({
-                embed: client.utils.embed(title, '_This message will self-destruct in 90 seconds._ :boom:', fields)
-            })).delete(90000);
-        });
+const settings = require('../config.json');
+exports.run = (client, message, args) => {
+  if (!args[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(`[Cryptide Commands]\n\n[Use ${settings.prefix}help <command> for information!]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: 'ini'});
+  } else {
+    let command = args[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.channel.send("", {embed: {
+        color: 15400990,
+            title: `Command Help:`,
+            description: `Command Name: \`${command.help.name}\` \nDescription: ${command.help.description}\nUsage: ${settings.prefix}${command.help.usage}`,
+          }});
     } else {
-        let categories = client.commands.categories().sort();
-        (await msg.edit({
-            embed: client.utils.embed(title, stripIndents`
-            **Available categories:**
-            ${categories.map(c => `- __${c}__`).join('\n')}
-
-            **Usage:**
-            Do \`${client.config.prefix}help category <name>\` for a list of commands in a specific category.
-            Do \`${client.config.prefix}help all\` for a list of every command available in this client.
-            Do \`${client.config.prefix}help <command>\` for **extended** command help and command options.`)
-        })).delete(15000);
+      message.channel.send("", {embed: {
+        color: 15400990,
+            title: `Non-Existant Command!`,
+            description: `I was unable to find a command by the name of: \`${args[0]}\``,
+          }});
     }
+  }
 };
 
-const getHelp = (client, command, single) => {
-
-    let description = stripIndents`
-        **Usage:** \`${client.config.prefix}${command.info.usage || command.info.name}\`
-        **Description:** ${command.info.description || '<no description>'}
-        **Category:** __${command.info.category}__`;
-
-    if (command.info.credits)
-        description += `\n**Credits:** *${command.info.credits}*`;
-
-    if (single && command.info.examples)
-        description += `\n**Examples:**\n${command.info.examples.map(example => `\`${client.config.prefix}${example}\``).join('\n')}`;
-
-    if (single && command.info.options instanceof Array) {
-        let options = command.info.options.map(option => {
-            return stripIndents`
-            **${option.name}**
-            *Usage:* \`${option.usage || option.name}\`
-            *Description:* ${option.description}
-            `;
-        });
-        description += `\n**Options:**\n\n${options.join('\n\n')}`;
-    }
-
-    return {
-        name: single ? '\u200b' : command.info.name,
-        value: description
-    };
+exports.conf = {
+  aliases: [],
+  permLevel: 0
 };
 
-exports.info = {
-    name: 'help',
-    usage: 'help all|[command]|[category <name>]',
-    description: 'Shows you help for all commands or just a single command'
+exports.help = {
+  name: 'help',
+  description: 'Displays available commands.',
+  usage: 'help <command>'
 };
