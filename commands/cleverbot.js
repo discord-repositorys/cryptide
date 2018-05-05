da84e8ba9efeb89a25943394bc5f3da0adccc534
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-05 14:21:51 -0400

    typing

--- commands/cleverbot.js
+++ commands/cleverbot.js
@@ -1,10 +1,12 @@
 exports.run = (client, message, args) => {
     const cleverbot = require("cleverbot.io");
     const clever = new cleverbot(process.env.CLEVERBOT_USER, process.env.CLEVERBOT_APIKEY);
+    message.channel.startTyping();
     clever.setNick("cryptide");
     clever.create(function(err, session) {
         clever.ask(args.join(' '), function(err, res) {
             message.channel.send(res);
+            message.channel.stopTyping();
         });
     });
 }
