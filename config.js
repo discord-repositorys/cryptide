8a6659e9de93968843e4818d8ce47476f516a591
ur-moms-a-trap21 <mcalts71@gmail.com>
2018-05-06 12:14:32 -0400

    worked

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ const config = {
 
   "defaultSettings" : {
     "prefix": "d.",
+    "systemNotice": "true" // This gives a notice when a user tries to run a command that they do not have permission to use.
   },
 
 
