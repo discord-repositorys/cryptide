8fe7596ac27e971045f144c90d74abf10ad7852b
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-06 15:11:58 -0400

    lol

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -16,7 +16,6 @@ exports.run = (client, message, args, level) => {
 .addField('Presence Game', message.author.presence.activity.name)
 .addField('Tag', message.author.tag) 
 .addField('Username', message.author.username) 
-.addField('Nick Name', message.guild.member(target).displayName) 
 
 .setFooter('Requested By', message.author.tag) 
 .setTimestamp() 
