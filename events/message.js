66632a7596eff9b1654333796f9fb79692ce4cde
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-05-08 01:18:19 +0300

    lol

--- events/message.js
+++ events/message.js
@@ -1,25 +1,36 @@
+const Prefix = require("../models/prefix.js");
 // The MESSAGE event runs anytime a message is received
 // Note that due to the binding of client to every event, every event
 // goes `client, other, args` when this function is run.
 
-module.exports = (client, message) => {
+module.exports = async(client, message) => {
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
   if (message.author.bot) return;
 
   // Grab the settings for this server from Enmap.
   // If there is no guild, get default conf (DMs)
-  const settings = message.settings = client.getGuildSettings(message.guild);
+  let prefix;
+  try {
+    if(!message.guild) prefix = "d.";
+    else { // ugly af if/else lol
+      const g = Prefix.findById(message.guild.id);
+      if(g && g.prefix) prefix = g.prefix;
+      else prefix = "d.";
+    } catch(err) {
+      console.error(err);
+    }
+  }
 
   // Also good practice to ignore any message that does not start with our prefix,
   // which is set in the configuration file.
-  if (message.content.indexOf(settings.prefix) !== 0) return;
+  if (message.content.indexOf(prefix) !== 0) return;
 
   // Here we separate our "command" name, and our "arguments" for the command.
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   // Get the user or member's permission level from the elevation
