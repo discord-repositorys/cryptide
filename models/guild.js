6cf3f6c4dc96bb2fe2edf3672c56349f8cf520d9
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-05-08 00:28:10 +0300

    Step 1

---
+++ models/guild.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const GuildSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  prefix: { type: String, required: true }
+});
+
+// name has to be singular, mongoose will make it plural
+// and create a collection.
+const guildModel = mongoose.model("guild", GuildSchema);
