1e53f24f766f01e9d9c4f5770db0b826b0622fec
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-07 18:50:12 -0400

    remove one

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, [prefix], level) => {
   if (!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.channel.send("Sorry you are not an Administrator so you can't use this command. Administrators are the people who possess the `Manage_Server` permission.")
   if(!prefix) return message.reply("Mention a prefix!");
   try {
-    await Guild.findOneByIdAndUpdate(message.guild.id, { prefix }, { upsert: true });
+    await Guild.findByIdAndUpdate(message.guild.id, { prefix }, { upsert: true });
     message.channel.send(`Changed prefix to \`${prefix}\``);
   } catch(err) {
     console.error(err);
