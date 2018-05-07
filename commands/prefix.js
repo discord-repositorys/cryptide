1a7ed520c291ed4d2c9bf8a81dd6e7c6f87d2132
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-05-08 01:02:10 +0300

    exports.run not module.exports :facepalm: oof

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,6 @@
 const Guild = require("../models/guild.js");
 
-module.exports = async (client, message, [prefix], level) => {
+exports.run = async (client, message, [prefix], level) => {
   if (!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.channel.send("Sorry sir you are not an Administrator so you can't use this command. Administrators are the people who possess the `Manage_Server` permission.")
   if(!prefix) return message.reply("Mention a prefix!");
   try {
