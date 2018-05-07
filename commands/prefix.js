b332942be5027593d087f007b798934bffa8824a
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-07 17:55:20 -0400

    fix prefix pls?

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,7 @@
 const Guild = require("../models/guild.js");
 
 module.exports = async (client, message, [prefix], level) => {
+  if (!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.channel.send("Sorry sir you are not an Administrator so you can't use this command. Administrators are the people who possess the `Manage_Server` permission.")
   if(!prefix) return message.reply("Mention a prefix!");
   try {
     await Guild.findOneByIdAndUpdate(message.guild.id, { prefix }, { upsert: true });
@@ -13,7 +14,7 @@ module.exports = async (client, message, [prefix], level) => {
 
 exports.conf = {
   aliases: [],
-  permLevel: "Moderator"
+  permLevel: "User"
 };
       
 exports.help = {
