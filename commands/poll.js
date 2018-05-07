9873b2ec834ed8269609f12459640242297d351e
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-07 17:36:35 -0400

    add poles command

--- commands/poll.js
+++ commands/poll.js
@@ -10,15 +10,15 @@ exports.run = async (client, message, args, level) => {
     .setFooter('React to vote on Poll!')
     .setColor('#7289DA')
     const pollTitle = await message.channel.send({ embed });
-      await pollTitle.react(`ðŸ‘`);
-      await pollTitle.react(`ðŸ‘Ž`);
+      await pollTitle.react(`:thumbsdown:`);
+      await pollTitle.react(`:thumbsup:`);
   
-    const filter = (reaction) => reaction.emoji.name === 'ðŸ‘';
+    const filter = (reaction) => reaction.emoji.name === ':thumbsdown:';
     const collector = pollTitle.createReactionCollector(filter, { time: 15000 });
       collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
       collector.on('end', collected => console.log(`Collected ${collected.size} items`));
   
-    const filter1 = (reaction) => reaction.emoji.name === 'ðŸ‘Ž';
+    const filter1 = (reaction) => reaction.emoji.name === ':thumbsup: ';
     const collector1 = pollTitle.createReactionCollector(filter1, { time: 15000 });
       collector1.on('collect', r => console.log(`Collected ${r.emoji.name}`));
       collector1.on('end', collected => console.log(`Collected ${collected.size} items`));
