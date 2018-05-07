899671d082c17346e671185746b361b0c08e5cf5
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-07 18:18:59 -0400

    revert

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,10 +1,10 @@
-const prefix = require("../models/prefix.js");
+const Guild = require("../models/guild.js");
 
 exports.run = async (client, message, [prefix], level) => {
   if (!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.channel.send("Sorry you are not an Administrator so you can't use this command. Administrators are the people who possess the `Manage_Server` permission.")
   if(!prefix) return message.reply("Mention a prefix!");
   try {
-    await prefix.findByIdAndUpdate(message.guild.id, { prefix }, { upsert: true });
+    await Guild.findOneByIdAndUpdate(message.guild.id, { prefix }, { upsert: true });
     message.channel.send(`Changed prefix to \`${prefix}\``);
   } catch(err) {
     console.error(err);
--- models/prefix.js
+++ models/prefix.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const prefixSchema = new mongoose.Schema({
+const GuildSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   prefix: { type: String, required: true }
 });
 
 // name has to be singular, mongoose will make it plural
 // and create a collection.
-const prefixModel = mongoose.model("prefix", prefixSchema);
-module.exports = prefixModel;
+const guildModel = mongoose.model("guild", GuildSchema);
+module.exports = guildModel;
