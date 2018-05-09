12b75371788a7d23fef658ef6430c9a6f4a77e2c
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-08 22:22:41 -0400

    helptest

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,42 @@
-const settings = require('../events/message.js');
-exports.run = (client, message, args) => {
-  if (!args[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`[Cryptide Commands]\n\n[Use d.help <command> for information!]\n\n${client.commands.map(c => `${message.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`,{ code: "ascii", split: "\n" });
-  } else {
-    let command = args[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.channel.send("", {embed: {
-        color: 15400990,
-            title: `Command Help:`,
-            description: `Command Name: \`${command.help.name}\` \nDescription: ${command.help.description}\nUsage: ${message.prefix}${command.help.usage}`,
-          }});
+exports.run = (client, message, args, level) => {
+    if (!args[0]) {
+      const myCommands = client.commands;
+
+      const commandNames = myCommands.keyArray();
+      const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+  
+      let currentCategory = "";
+      let output = `= Command List =\n\n[Use ${message.prefix}help <commandname> for details]\n`;
+      const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+      sorted.forEach( c => {
+        const cat = c.help.category;
+        if (currentCategory !== cat) {
+          output += `\u200b\n== ${cat} ==\n`;
+          currentCategory = cat;
+        }
+        output += `${message.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+      });
+      message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
     } else {
-      message.channel.send("", {embed: {
-        color: 15400990,
-            title: `Non-Existant Command!`,
-            description: `I was unable to find a command by the name of: \`${args[0]}\``,
-          }});
+      let command = args[0];
+      if (client.commands.has(command)) {
+        command = client.commands.get(command);
+        if (level < client.levelCache[command.conf.permLevel]) return;
+        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
+      }
     }
-  }
-};
-
-exports.conf = {
-  aliases: [],
-  permLevel: "User"
-};
-
-exports.help = {
-  name: 'help',
-  description: 'Displays available commands.',
-  usage: 'help <command>'
-};
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: "help",
+    category: "Misc",
+    description: "Displays all the available commands for your permission level.",
+    usage: "help [command]"
+  };
