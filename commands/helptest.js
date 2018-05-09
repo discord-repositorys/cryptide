33017aa652d0aa8fb7b603c880ac2fdd6d0f2d2f
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-08 22:18:37 -0400

    helptest

--- commands/helptest.js
+++ commands/helptest.js
@@ -17,7 +17,7 @@ exports.run = (client, message, args, level) => {
       const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
   
       let currentCategory = "";
-      let output = `= Command List =\n\n[Use ${client.config.prefix}help <commandname> for details]\n`;
+      let output = `= Command List =\n\n[Use ${message.prefix}help <commandname> for details]\n`;
       const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
       sorted.forEach( c => {
         const cat = c.help.category;
@@ -25,7 +25,7 @@ exports.run = (client, message, args, level) => {
           output += `\u200b\n== ${cat} ==\n`;
           currentCategory = cat;
         }
-        output += `${client.config.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+        output += `${message.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
       });
       message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
     } else {
