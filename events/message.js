df3d80ccc21c8c3c0da2c45d13261e4c17d8dc8e
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-10 20:35:01 -0400

    fix check

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const settings = require("../config.js")
 const Prefix = require("../models/guild.js");
 const ping = client.emojis.find("name", "angeryping");
 const deal = client.emojis.find("name", "DealWithIt");
