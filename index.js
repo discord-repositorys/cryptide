60642ccac1cbfc54dba5663e7ef7dc4c3e50c3a0
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-12 19:46:24 -0400

    sync back

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ require("./modules/functions.js")(client);
 
 
 
-
 function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
