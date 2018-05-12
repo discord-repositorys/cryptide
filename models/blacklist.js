064f1e9bdec33f3b919a41884c65209c386f1009
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-11 20:31:02 -0400

    will it work?

--- models/blacklist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BlacklistSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  user: { type: String, required: true }
-});
-
-const blacklistModel = mongoose.model("blacklist", BlacklistSchema);
-module.exports = blacklistModel;
