4776532d1cb45ab523f85a4de32d0065ee2ac381
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-11 20:12:35 -0400

    will it work?

--- models/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const guildSchema = new mongoose.Schema({
-  _id: { type: String, required: true }, // this is server id
-  prefix: { type: String, required: true }, // custom prefix.
-  starboardChannel: { type: String, default: "off" } // anything else u want
-});
-const guildModel = mongoose.model("guild", guildSchema);
-module.exports = guildModel;
