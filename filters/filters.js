136016e39882777535a4edfe97bdf550451d411b
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-13 18:39:24 -0400

    add

--- filters/filters.js
+++ filters/filters.js
@@ -160,6 +160,7 @@ exports.convolute = (canvas, weights) => {
     const { data } = imageData;
     const { width, height } = canvas;
 
+    
     // go through the destination image pixels
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
