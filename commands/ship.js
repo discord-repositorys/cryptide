978dbb5560f3b98a8a4fbe14ee1536cabbba35f8
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-13 12:16:47 -0400

    add ship

---
+++ commands/ship.js
@@ -0,0 +1,66 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let user1 = args[0];
+if(!user1) return message.reply("Please mention two users to ship!");
+let user2 = args[1];
+if(!user2) return message.reply("Please mention a second user!");
+
+let result = Math.floor(Math.random() * 100) + 1;
+
+message.channel.send(`❣ **MATCHMAKING** ❣\n🔻 ${user1} \n🔺 ${user2}`);
+
+let aEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Awful 😭`);
+
+let bEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Bad 😢`);
+
+let ltaEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Worse Than Average 😐`);
+
+let nbEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Not Bad 🙂`);
+
+let pgEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Pretty Good 😃`);
+
+let goEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Good 😄`);
+
+let gEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Great 😊`);
+
+let pEmbed = new MessageEmbed()
+.setColor("#ff7fd0")
+.setTitle(`${result}% Perfect 😍`);
+
+if(result <= 15) return message.channel.send(aEmbed)
+if(result <= 30) return message.channel.send(bEmbed)
+if(result <= 50) return message.channel.send(ltaEmbed)
+if(result <= 60) return message.channel.send(nbEmbed)
+if(result <= 70) return message.channel.send(pgEmbed)
+if(result <= 80) return message.channel.send(goEmbed)
+if(result <= 90) return message.channel.send(gEmbed)
+if(result >= 90) return message.channel.send(pEmbed)
+}
+exports.conf = {
+    aliases: [],
+    permLevel: "User"
+  };
+      
+  exports.help = {
+    name: 'ship',
+    category: "Fun",
+    description: 'Ship two people together',
+    usage: 'ship'
+  };
